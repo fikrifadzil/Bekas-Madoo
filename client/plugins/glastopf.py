8d993664032cfd3313f484cee4681b5c93e30477
root <fikri.fadzil@gmail.com>
2014-02-07 01:31:45 +0800

    Bugs fix

--- client/plugins/glastopf.py
+++ client/plugins/glastopf.py
@@ -4,9 +4,12 @@ import requests
 from libs import main
 
 def migrate(config):
-	result = main.select(config, 'SELECT * FROM events')
+	
+	# Grab data from glastopf.events
+	
+	result = main.select(config['honeypot']['glastopf'], 'SELECT * FROM events')
 	jeson  = json.dumps( result, separators=(',',':'))
-	status = main.knockknock('glastopf_events', jeson, config)
+	status = main.knockknock('glastopf_events', jeson, config['api_url'])
 	
 	if status:
-		main.delete(config, 'DELETE FROM events')
+		main.delete(config['honeypot']['glastopf'], 'DELETE FROM events')
